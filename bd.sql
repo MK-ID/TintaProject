20a65cca0916ad4a925a3d9802376e389e81c9e0
Luis Mikhail <luispalominopaucar@hotmail.com>
2022-06-16 19:30:55 -0500

    corrigiendo archivos de la base de datos

--- bd.sql
+++ bd.sql
@@ -6,27 +6,27 @@ CREATE TABLE tPersonas
     Edad TINYINT,
     Unidad VARCHAR(50)
 );
-INSERT into tPersonas VALUES ('72663013','Palomino','Luis','24','Logistica');
+INSERT into tPersonas VALUES ('43234356','Rey Sanchez','Marcela','24','Logistica');
 
-INSERT into tPersonas VALUES ('12365489','Paucar','Mikhail','24','RRHH');
+INSERT into tPersonas VALUES ('67545654','Carvajal Vargas','Alexander','24','RRHH');
 
-INSERT into tPersonas VALUES ('89654825','Delgado','Jheremy','29','Finanzas');
+INSERT into tPersonas VALUES ('09789658','Acero Caro','Catalina','29','Finanzas');
 
-INSERT into tPersonas VALUES ('36954625','Rivas','Almendra','24','RRHH');
+INSERT into tPersonas VALUES ('98567843','Villa Monroy','Felipe','24','RRHH');
 
-INSERT into tPersonas VALUES ('21159865','Ortiz','Marcelo','24','RRHH');
-
-
-INSERT into tPersonas VALUES ('32569587','Gareca','Ricardo','24','Logistica');
+INSERT into tPersonas VALUES ('67545623','Rocha Garcia','Maria','24','RRHH');
 
 
-INSERT into tPersonas VALUES ('98485254','Salinas','Ludvig','20','RRHH');
+INSERT into tPersonas VALUES ('32569587','Castaño Contreras','Didier','24','Logistica');
 
 
-INSERT into tPersonas VALUES ('98695415','Mansilla','Jampier','27','Finanzas');
+INSERT into tPersonas VALUES ('98567832','Mogollon Pacgon','Felipe','20','RRHH');
 
 
-INSERT into tPersonas VALUES ('32654821','Utrilla','Daryl','24','Logistica');
+INSERT into tPersonas VALUES ('09123212','Rocha Garcia','Lisseth','27','Finanzas');
 
 
-INSERT into tPersonas VALUES ('89654852','Laguna','Jorge','28','Finanzas');
+INSERT into tPersonas VALUES ('78549568','Mahecha Piñeros','Angela','24','Logistica');
+
+
+INSERT into tPersonas VALUES ('56545634','Blanco Concha','Angelica','28','Finanzas');
